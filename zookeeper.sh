5f71c558607152f6e4e654cd181c9224766486f5
Slava Vladyshevsky <vyacheslav.vladyshevsky@ibm.com>
2019-02-05 15:03:46 +0100

    Whitelisted "ruok" and "stat" commands, which are disabled by default. Disabled Admin HTTP server. Properly generating server.x list out of ENV variables. Printing ENV variables and config file during the startup for debugging purposes.

--- zookeeper.sh
+++ zookeeper.sh
@@ -2,16 +2,16 @@
 
 # enabling strict mode http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
-# IFS=$'\n\t'
 
 # generic JVM settings you may want to add
 JVM_OPTS=${JVM_OPTS:-""}
 CLASSPATH=${CLASSPATH:-""}
+ZK_SERVERS=${ZK_SERVERS:-"localhost:2888:3888"}
 
 # generate the config only if it doesn't exist
 if [ ! -f "$ZOOCFGDIR/$ZOOCFG" ]; then
     cat <<-EOT >"$ZOOCFGDIR/$ZOOCFG"
-clientPort=$ZK_PORT
+clientPort=$ZK_CLIENT_PORT
 dataDir=$ZOODATADIR
 dataLogDir=$ZOOLOGSDIR
 
@@ -19,14 +19,20 @@ tickTime=$ZK_TICK_TIME
 initLimit=$ZK_INIT_LIMIT
 syncLimit=$ZK_SYNC_LIMIT
 
-autopurge.snapRetainCount=$ZK_AUTOPURGE_SNAPRETAINCOUNT
-autopurge.purgeInterval=$ZK_AUTOPURGE_PURGEINTERVAL
+# whitelisting specific commands
+4lw.commands.whitelist=stat, ruok
+# disabling HTTP server
+admin.enableServer=false
+autopurge.snapRetainCount=$ZK_AUTOPURGE_SNAP_RETAIN_COUNT
+autopurge.purgeInterval=$ZK_AUTOPURGE_PURGE_INTERVAL
 maxClientCnxns=$ZK_MAX_CLIENT_CNXNS
 standaloneEnabled=$ZK_STANDALONE_ENABLED
+
 		EOT
 
-    for server in ${ZK_SERVERS:-"server.1=localhost:2888:3888;$ZK_PORT"}; do
-        echo "$server" >> "$ZOOCFGDIR/$ZOOCFG"
+    idx=1
+    for s in $(echo $ZK_SERVERS | tr ';' '\n'); do
+        echo server.$((idx++))=$s >> "$ZOOCFGDIR/$ZOOCFG"
     done
 fi
 
@@ -43,6 +49,12 @@ export JVM_HEAP_OPTS="-Xms512M -Xmx512M"
 # export JMX_PORT=5555
 # export JVM_GC_LOG=/var/log/gc.log
 export LOG4J_CONF="$ZOOCFGDIR/log4j.properties"
+
+echo "===[ Environment   ]==========================================================="
+env
+echo "===[ Configuration ]==========================================================="
+cat "$ZOOCFGDIR/$ZOOCFG"
+echo "===[ Zookeeper log ]==========================================================="
 
 # Launch mode
 exec jvm.sh org.apache.zookeeper.server.quorum.QuorumPeerMain "$ZOOCFGDIR/$ZOOCFG"
