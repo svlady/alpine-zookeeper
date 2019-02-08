c33c109a76aee3ce21b8455d9c48002a86a2dfa1
Slava Vladyshevsky <vyacheslav.vladyshevsky@ibm.com>
2019-02-08 09:33:11 +0100

    Improved shell code robustness.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -e
+# enabling strict mode http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
 
 # created files should always be group writable too
 umask 0002
 
-# Allow the container to be started with `--user`
-if [[ "$1" = 'zookeeper.sh' && "$(id -u)" = '0' ]]; then
-    chown -R "$ZK_USER:0" "$ZOODATADIR" "$ZOOLOGSDIR" "$ZOOCFGDIR"
-    exec su-exec "$ZK_USER" "$0" "$@"
+# allow the container to be started with `--user`
+if [[ "$1" = 'bootstrap.sh' && "$(id -u)" = '0' ]]; then
+  chown -R "$ZK_USER:0" $ZK_HOME/data $ZK_HOME/logs $ZK_HOME/conf
+  # dropping privileges
+  exec su-exec "$ZK_USER" "$0" "$@"
 fi
 
 exec "$@"
